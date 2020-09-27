3c4eeb1df65ec2ee03edcb90fead677014536e88
Кирилл Яценко <kirillyat@gmail.com>
2020-09-28 01:02:12 +0300

    Update trip.sql

--- trip.sql
+++ trip.sql
@@ -1,11 +1,11 @@
 --TRIP SQL BILDER
 
 --CLEAR CURRENT DATABASE FOR NEW TABLES AND TYPES
-DROP TABLE IF EXISTS trip_movement;
+DROP TABLE IF EXISTS movements;
 DROP TABLE IF EXISTS trip_client;
 DROP TABLE IF EXISTS trip_hotel;
+
 DROP TABLE IF EXISTS trips;
-DROP TABLE IF EXISTS movements;
 DROP TABLE IF EXISTS clients_documents;
 DROP TABLE IF EXISTS clients;
 DROP TABLE IF EXISTS hotels;
@@ -20,7 +20,7 @@ CREATE TYPE SEX_ENUM AS ENUM('male', 'female');
 
 CREATE TABLE trips (
     trip_id SERIAL,
-    cost integer CHECK (cost > 0),
+    cost money CHECK (cost::numeric > 0.0),
     begin_date date,
     end_date date CHECK (end_date >= begin_date),
 
@@ -31,8 +31,8 @@ CREATE TABLE clients (
     client_id SERIAL,
     full_name text NOT NULL,
     sex sex_enum NOT NULL,
-    email varchar(50) CHECK(email LIKE '%_@__%.__%' AND email NOT LIKE '%[!@#$%^&*(){};:?/%~§±+=]%') NOT NULL,
-    phone varchar(12) CHECK (phone not like '%[^0-9]%'),
+    email varchar(360) CHECK(email LIKE '%_@__%.__%' AND email NOT LIKE '%[!@#$%^&*(){};:?/%~§±+=]%') NOT NULL,
+    phone varchar(20) CHECK (phone not like '%[^0-9]%'),
 
     PRIMARY KEY(client_id)
 );
@@ -44,31 +44,30 @@ CREATE TABLE clients_documents (
     local_passport text,
     birth_certificate text,
 
-    FOREIGN KEY (client_id) REFERENCES clients(client_id)
+    FOREIGN KEY (client_id) REFERENCES clients(client_id) ON DELETE CASCADE,
+    PRIMARY KEY (client_id)
 );
 
 CREATE TABLE trip_client (
     client_id integer,
     trip_id integer,
 
-    FOREIGN KEY (client_id) REFERENCES clients(client_id),
-    FOREIGN KEY (trip_id) REFERENCES trips(trip_id)
+    FOREIGN KEY (client_id) REFERENCES clients(client_id) ON DELETE CASCADE,
+    FOREIGN KEY (trip_id) REFERENCES trips(trip_id) ON DELETE CASCADE,
+
+    PRIMARY KEY (trip_id, client_id)
 );
 
 CREATE TABLE movements (
+
+    trip_id integer,
     movement_id SERIAL,
     departure_time timestamp NOT NULL,
     departure_address text NOT NULL,
 
+    FOREIGN KEY (trip_id) REFERENCES trips(trip_id) ON DELETE CASCADE,
+
     PRIMARY KEY(movement_id)
-);
-
-CREATE TABLE trip_movement (
-    trip_id integer,
-    movement_id integer,
-
-    FOREIGN KEY (trip_id) REFERENCES trips(trip_id),
-    FOREIGN KEY (movement_id) REFERENCES movements(movement_id)
 );
 
 
@@ -76,8 +75,8 @@ CREATE TABLE hotels (
     hotel_id SERIAL,
     name text,
     address text,
-    email varchar(50) CHECK(email LIKE '%_@__%.__%' AND email NOT LIKE '%[!@#$%^&*(){};:?/%~§±+=]%'),
-    phone varchar(12) CHECK (phone not like '%[^0-9]%'),
+    email varchar(360) CHECK(email LIKE '%_@__%.__%' AND email NOT LIKE '%[!@#$%^&*(){};:?/%~§±+=]%') NOT NULL,
+    phone varchar(20) CHECK (phone not like '%[^0-9]%'),
 
     PRIMARY KEY(hotel_id)
 );
@@ -89,8 +88,9 @@ CREATE TABLE trip_hotel (
     begin_date date,
     end_date date CHECK (end_date >= begin_date),
 
-    FOREIGN KEY (trip_id) REFERENCES trips(trip_id),
-    FOREIGN KEY (hotel_id) REFERENCES hotels(hotel_id)
+    FOREIGN KEY (trip_id) REFERENCES trips(trip_id) ON DELETE CASCADE,
+    FOREIGN KEY (hotel_id) REFERENCES hotels(hotel_id),
+    PRIMARY KEY (trip_id, hotel_id)
 );
 
 
@@ -141,11 +141,11 @@ VALUES
 INSERT INTO
 	trips(cost, begin_date, end_date)
 VALUES
-	(10000, '2020-11-14', '2021-12-15'),--1
-	(1000, '2020-10-01', '2020-10-10'),--2
-	(900, '2020-12-12', '2020-12-22'),--3
-	(2200, '2022-02-12', '2022-02-20'),--4
-	(6000, '2021-06-01', '2021-07-01')--5
+	(100000, '2020-11-14', '2021-12-15'),--1
+	(50000, '2020-10-01', '2020-10-10'),--2
+	(90000, '2020-12-12', '2020-12-22'),--3
+	(122000, '2022-02-12', '2022-02-20'),--4
+	(60000, '2021-06-01', '2021-07-01')--5
 ;
 
 INSERT INTO
@@ -173,54 +173,50 @@ VALUES
 ;
 
 INSERT INTO
-    movements(departure_time, departure_address)
+    movements(trip_id, departure_time, departure_address)
 VALUES
-    ('2020-11-14 09:00:00 +3', 'seremetevo moscow russia'),
-    ('2021-01-01 21:00:00 +0', 'hitrow london UK'),
-    ('2021-12-15 08:30:00 +1', 'orsa paris france'),
-    ('2020-10-01 18:30:00 +7', 'omsk airport russia'),
-    ('2020-10-10 06:30:00 +3', 'seremetevo moscow russia'),
-    ('2020-12-12 08:30:00 +1', 'pulkovo sankt piterburg'),
-    ('2020-12-18 20:30:00 +5', 'sochi adler airport russia'),
-    ('2020-12-22 20:00:00 +9', 'railway cantrale Madrid, SPAIN'),
-    ('2022-02-12 10:30:00 +6', 'san-francisco center bus station usa'),
-    ('2022-02-20 18:10:00 +4', 'las-vegas shattel bus stop#12 usa'),
-    ('2021-06-01 08:10:00 +3', 'orsa paris france'),
-    ('2021-06-21 23:50:00 +4', 'antaliya center railway'),
-    ('2021-07-01 18:10:00 +4', 'vnukovo airport russia')
+    (1,'2020-11-14 09:00:00 +3', 'seremetevo moscow russia'),
+    (1,'2021-01-01 21:00:00 +0', 'hitrow london UK'),
+    (1,'2021-12-15 08:30:00 +1', 'orsa paris france'),
+    (2,'2020-10-01 18:30:00 +7', 'omsk airport russia'),
+    (2,'2020-10-10 06:30:00 +3', 'seremetevo moscow russia'),
+    (3,'2020-12-12 08:30:00 +1', 'pulkovo sankt piterburg'),
+    (3,'2020-12-18 20:30:00 +5', 'sochi adler airport russia'),
+    (3,'2020-12-22 20:00:00 +9', 'railway cantrale Madrid, SPAIN'),
+    (4,'2022-02-12 10:30:00 +6', 'san-francisco center bus station usa'),
+    (4,'2022-02-20 18:10:00 +4', 'las-vegas shattel bus stop#12 usa'),
+    (5,'2021-06-01 08:10:00 +3', 'orsa paris france'),
+    (5,'2021-06-21 23:50:00 +4', 'antaliya center railway'),
+    (5,'2021-07-01 18:10:00 +4', 'vnukovo airport russia')
 ;
 
-INSERT INTO
-    trip_movement(trip_id, movement_id)
-VALUES
-    (1, 1), (1, 2), (1,3),
-    (2, 4), (2, 5),
-    (3, 6), (3, 7), (3, 8),
-    (4, 9), (4, 10),
-    (5, 11), (5, 12), (5, 13)
-;
 
 
 
 INSERT INTO clients(full_name, sex, email, phone) VALUES
-	('SIDOROV ALEXANDER VLADIMIROVITCH', 'male', 'sidr@yandex.ru', '+70001231212');
+	('SIDOROV ALEXANDER VLADIMIROVITCH', 'male', 'sidr@yandex.ru', '+70001231212') RETURNING *;
 
 INSERT INTO clients_documents(client_id, international_passport, local_passport, birth_certificate) VALUES
     (11, '771277112', '213309AAA', NULL); --11
 
 INSERT INTO trips(cost, begin_date, end_date) VALUES
-	(1000, '2020-11-14', '2021-12-15'); --6
+	(100000, '2020-11-14', '2021-12-15')
+RETURNING *; --6
+
+INSERT INTO trip_client(client_id, trip_id) VALUES (11, 6);
 
 INSERT INTO trip_hotel(trip_id, hotel_id, begin_date, end_date) VALUES
     (6, 1, '2020-12-01', '2021-07-01'),
 	(6, 5, '2021-07-01', '2021-12-15');
 
-INSERT INTO trip_client(client_id, trip_id) VALUES (11, 6);
 
-INSERT INTO movements(departure_time, departure_address) VALUES
-    ('2020-12-01 19:00:00 +3', 'seremetevo moscow russia'),--14
-    ('2021-07-01 11:00:00 +0', 'sochi adler russia'),--15
-	('2021-12-15 13:00:00 +0', 'madrid spain espania');--16
+INSERT INTO movements(trip_id, departure_time, departure_address) VALUES
+    (6,'2020-12-01 19:00:00 +3', 'seremetevo moscow russia'),--14
+    (6,'2021-07-01 11:00:00 +0', 'sochi adler russia'),--15
+	(6,'2021-12-15 13:00:00 +0', 'madrid spain espania')
+	RETURNING *;--16
 
-INSERT INTO trip_movement(trip_id, movement_id) VALUES
-    (6, 14), (6, 15), (6, 16);
+
+
+
+--delete from trips where trip_id=6
